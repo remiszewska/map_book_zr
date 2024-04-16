43df52abb75e7e05d1c0f0ab450e26c1d3caf432
remiszewska <163434600+remiszewska@users.noreply.github.com>
2024-04-16 18:53:28 +0200

    wip: add new menu function, add user search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from models.data_source import users
-from utils.crud import read_friends, add_user
-
-
+from utils.crud import read_friends, add_user, search_user
 
 if __name__ == '__main__':
     while True:
@@ -9,6 +7,7 @@ if __name__ == '__main__':
         print("0. Exit")
         print("1. Read a list of friends")
         print("2. Add new user")
+        print("3. Search user")
         menu_option = input("Choose an option: ")
         if menu_option == "0":
             break
@@ -16,5 +15,5 @@ if __name__ == '__main__':
             read_friends(users)
         if menu_option == "2":
             add_user(users)
-
-
+        if menu_option == "3":
+            search_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -9,3 +9,9 @@ def add_user(lista: list) -> None:
     liczba_postow = int(input("Podaj liczbe postów użytkownika: "))
     new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postow, }
     lista.append(new_user)
+
+def search_user(users: list):
+    imie = input("Podaj imię: ")
+    for user in users:
+        if user["name"] == imie:
+            print(user)
