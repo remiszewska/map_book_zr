699a4f56a7e2712ede6f6a0a1551a1ea48072e6c
remiszewska <163434600+remiszewska@users.noreply.github.com>
2024-04-16 17:02:50 +0200

    wip: read code refactor to function

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ users: list = [
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
     {"name": "Hubert", "surname": "Sybilski", "posts": 1000000, },
     {"name": "Adrian", "surname": "Dobrzański", "posts": 1, },
+
 ]
 
-print("Informacje o twoich znajomych: ")
-for user in users:
-    print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+def read_friends(users: list)->None:
+    print("Informacje o twoich znajomych: ")
+    for user in users:
+        print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+
+read_friends(users)
