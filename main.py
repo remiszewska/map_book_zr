e4780309aac6451bce5cf43012858f66c45c1e39
remiszewska <163434600+remiszewska@users.noreply.github.com>
2024-04-16 16:52:59 +0200

    wip: update data structure to list of dict

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-imie_1: str = "Artur"
-imie_2: str = "Zuzanna"
-imie_3: str = "Kinga"
-imie_4: str = "Marysia"
-online_1: str = "online"
-online_2: str = "offline"
-online_3: str = "online"
-online_4: str = "online"
+users: list = [
 
-users: list[str] = [imie_1, imie_2, imie_3, imie_4]
-lista_online: list[str] = [online_1, online_2, online_3, online_4]
-print("Lista znajomych to: ")
-for index, user in enumerate(users):
-    print(f"\tTwój znajomy {user} jest {lista_online[index]}")
+    {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
+    {"name": "Hubert", "surname": "Sybilski", "posts": 1000000, },
+    {"name": "Adrian", "surname": "Dobrzański", "posts": 1, },
+]
 
+print("Informacje o twoich znajomych: ")
+for user in users:
+    print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
